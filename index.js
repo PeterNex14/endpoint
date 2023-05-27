961d2b09d3bfabdcbe349c48ee61f33ae890e55f
Peter Gabriel <peternex42@gmail.com>
2023-05-27 14:40:47 +0800

    name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   if (radius === undefined) {
     res.statusCode = 400;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Error: Tolong masukkan nilai ke dalam parameter radius di url.\n');
+    res.end('Error: Tolong masukkan nilai ke dalam parameter radius di url.\n\nNama : Peter Gabriel Sompotan\nNIM : 210211060245');
   } else {
     const area = Math.PI * radius ** 2;
     res.statusCode = 200;
